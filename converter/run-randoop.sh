6305e2a9826b4d13b1571f25a424517cbffadb6c
connerk <connerk@uw.edu>
2018-04-13 22:58:44 -0700

    added sh

---
+++ converter/run-randoop.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/connerknight/java-design-patterns.git
+cd java-design-patterns/converter
+java -classpath src/main/java:randoop-4.0.3/randoop-all-4.0.3.jar randoop.main.Main gentests --testclass=com.iluwatar.converter.User --time-limit=60 --minimize-error-test
+
